d8c4434f3dd05f6a84d24f9b8b4b01febd83fcac
Andres Montano <amontano@virginia.edu>
2010-02-08 10:25:11 -0600

    seems globalize is not overriding the load_path << method

--- init.rb
+++ init.rb
@@ -1,2 +1,3 @@
 # Include hook code here
-I18n.load_path << File.join(File.dirname(__FILE__), 'config', 'locales')
+# I18n.load_path << File.join(File.dirname(__FILE__), 'config', 'locales')
+I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config', 'locales', '**', '*.yml')]
