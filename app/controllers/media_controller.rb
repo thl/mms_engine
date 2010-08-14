8c2026b9c407a2c0543603c0b9dbfb26e5347ae0
Tom Benner <tbb4n@virginia.edu>
2010-08-14 13:26:00 -0400

    Using .fid for a Place URL in the Browse tab in MediaController#index

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -57,6 +57,8 @@ class MediaController < AclController
           @tab_options[:counts] = tab_counts_for_element(@element)
           @tab_options[:urls] = tab_urls_for_element(@element)
           @tab_options[:urls][:browse] = polymorphic_url(@element)
+          # Need to use .fid if the element is a Place
+          @tab_options[:urls][:browse] = place_url(@element.fid) if @element.class.name == 'Place'
           @medium_pages = Paginator.new self, @element.count_media(type), Medium::FULL_COLS * Medium::FULL_ROWS, params[:page]
           @media = @element.paged_media(@medium_pages.items_per_page, @medium_pages.current.offset, type)
           @pagination_params[element_name] = element_id
