5c3385111ef6838335d695334df975920338c30b
Andres Montano <amontano@virginia.edu>
2012-02-15 01:14:48 -0600

    expiring cache

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,6 +1,6 @@
 module Tree
   def descendants
-    Rails.cache.fetch("#{self.class.element_name}-#{self.id}/descendants") do
+    Rails.cache.fetch("#{self.class.element_name}-#{self.id}/descendants", :expires_in => 1.day) do
       children_to_be_processed = [self]
       all_children = Array.new
       while !children_to_be_processed.empty?
@@ -13,7 +13,7 @@ module Tree
   end
 
   def full_lineage(size = nil)
-    Rails.cache.fetch("#{self.class.element_name}-#{self.id}/full_lineage") do
+    Rails.cache.fetch("#{self.class.element_name}-#{self.id}/full_lineage", :expires_in => 1.day) do
       if (size==nil)
         lineage = (ancestors.reverse << self).collect{|e| e.title }
       else 
@@ -28,11 +28,11 @@ module Tree
   end
   
   def count_inherited_media(type = nil)
-    Rails.cache.fetch("#{self.class.element_name}-#{self.id}/count_inherited_media") { CachedCategoryCount.updated_count(self.id.to_i, type).count }
+    Rails.cache.fetch("#{self.class.element_name}-#{self.id}/count_inherited_media", :expires_in => 1.day) { CachedCategoryCount.updated_count(self.id.to_i, type).count }
   end
 
   def permalink
-    Rails.cache.fetch("#{self.class.element_name}-#{self.id}/permalink") do
+    Rails.cache.fetch("#{self.class.element_name}-#{self.id}/permalink", :expires_in => 1.day) do
       association = registered_theme_association
       association.nil? ? nil : association.permalink
     end
